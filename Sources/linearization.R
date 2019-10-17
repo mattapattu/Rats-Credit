64abc5951e17e37c8620cd43b496f341168944f0
mattapattu <mattapattu@gmail.com>
2019-10-17 17:36:34 +0200

    latest linearization

--- Sources/linearization.R
+++ Sources/linearization.R
@@ -7,32 +7,67 @@ plot.spikes.by.boxes=function(ses,enreg){
   trial = 0
   spiketrain = list()
   print("HERE1")
-  size =length(enreg[[ses]]$SPIKES[,"trial"])
+  size =length(enreg[[ses]]$SPIKES[,1])
   max_trial = enreg[[ses]]$POS[size,"trial"]
   spiketrain[[ses]]=list("trials"= c(1:max_trial))
   print("HERE2")
   for(idx in 1:max_trial){
     print("HERE3")
-     spiketrain[[ses]]$trials[idx] = list(cbind("boxname" = c("a","b","c","d","e","f","g","h","i")))
+     spiketrain[[ses]]$trials[idx] = list("boxname" = list("a","b","c","d","e","f","g","h","i"))
      sub_enreg <- subset(enreg[[ses]]$SPIKES,enreg[[ses]]$SPIKES[,"trial"] == idx) 
+     sub_enreg <- subset(sub_enreg,sub_enreg[,"neuron"] != 0)
      print("HERE4")
-     for(idx in 1:length(sub_enreg[,1])){
+     spiketrain[[ses]]$trials[idx] = list(cbind(spiketrain[[ses]]$trials[[idx]], Neuron1="0",Neuron2="0",Neuron3="0",Neuron4="0",Neuron5="0",Neuron6="0",Neuron7="0",Neuron8="0",Neuron9="0",Neuron10="0",Sum="0"))
+     for(sp in 1:length(sub_enreg[,1])){
        print("HERE5")
-       if(sub_enreg[idx,"neuron"] != "0"){
-         neuron_nb = sub_enreg[idx,"neuron"]
-         bool = neuron_nb %in% colnames(spiketrain[trial])
-         neuron = paste("Neuron", neuron_nb)
-         print("HERE6")
-         if(!bool){
-           spiketrain[[ses]]$trials[idx] = list(cbind(spiketrain[[ses]]$trials[[idx]], neuron))
-         }
-         boxname = enreg[[ses]]$SPIKES[idx,"boxName"]
-         spiketrain[[ses]]$trials[[idx]][1,neuron] =  spiketrain[[ses]]$trials[[idx]][1,neuron]
-         print("HERE7")
+       neuron_nb = sub_enreg[sp,"neuron"]
+       neuron = paste("Neuron", neuron_nb,sep = "")
+       boxname = sub_enreg[sp,"boxName"]
+       if(boxname == "a"){
+         spiketrain[[ses]]$trials[[idx]][1,neuron] =  as.numeric(spiketrain[[ses]]$trials[[idx]][1,neuron]) + 1
+         spiketrain[[ses]]$trials[[idx]][9,"Sum"] = as.numeric(spiketrain[[ses]]$trials[[idx]][1,"Sum"])+1  
+         
+       }else if(boxname == "b"){
+         spiketrain[[ses]]$trials[[idx]][2,neuron] =  as.numeric(spiketrain[[ses]]$trials[[idx]][2,neuron]) + 1
+         spiketrain[[ses]]$trials[[idx]][9,"Sum"] = as.numeric(spiketrain[[ses]]$trials[[idx]][2,"Sum"])+1
+         
+       }else if(boxname == "c"){
+         spiketrain[[ses]]$trials[[idx]][3,neuron] =  as.numeric(spiketrain[[ses]]$trials[[idx]][3,neuron]) + 1
+         spiketrain[[ses]]$trials[[idx]][9,"Sum"] = as.numeric(spiketrain[[ses]]$trials[[idx]][3,"Sum"])+1
+         
+       }else if(boxname == "d"){
+         spiketrain[[ses]]$trials[[idx]][4,neuron] =  as.numeric(spiketrain[[ses]]$trials[[idx]][4,neuron]) + 1
+         spiketrain[[ses]]$trials[[idx]][9,"Sum"] = as.numeric(spiketrain[[ses]]$trials[[idx]][4,"Sum"])+1 
+         
+       }else if(boxname == "e"){
+         spiketrain[[ses]]$trials[[idx]][5,neuron] =  as.numeric(spiketrain[[ses]]$trials[[idx]][5,neuron]) + 1
+         spiketrain[[ses]]$trials[[idx]][9,"Sum"] = as.numeric(spiketrain[[ses]]$trials[[idx]][5,"Sum"])+1
+         
+       }else if(boxname == "f"){
+         spiketrain[[ses]]$trials[[idx]][6,neuron] =  as.numeric(spiketrain[[ses]]$trials[[idx]][6,neuron]) + 1
+         spiketrain[[ses]]$trials[[idx]][9,"Sum"] = as.numeric(spiketrain[[ses]]$trials[[idx]][6,"Sum"])+1
+         
+       }else if(boxname == "g"){
+         spiketrain[[ses]]$trials[[idx]][7,neuron] =  as.numeric(spiketrain[[ses]]$trials[[idx]][7,neuron]) + 1
+         spiketrain[[ses]]$trials[[idx]][9,"Sum"] = as.numeric(spiketrain[[ses]]$trials[[idx]][7,"Sum"])+1
+         
+       }else if(boxname == "h"){
+         spiketrain[[ses]]$trials[[idx]][8,neuron] =  as.numeric(spiketrain[[ses]]$trials[[idx]][8,neuron]) + 1
+         spiketrain[[ses]]$trials[[idx]][9,"Sum"] = as.numeric(spiketrain[[ses]]$trials[[idx]][8,"Sum"])+1
+         
+       }else if(boxname == "i"){
+         spiketrain[[ses]]$trials[[idx]][9,neuron] =  as.numeric(spiketrain[[ses]]$trials[[idx]][9,neuron]) + 1
+         spiketrain[[ses]]$trials[[idx]][9,"Sum"] = as.numeric(spiketrain[[ses]]$trials[[idx]][9,"Sum"])+1
+         
        }
+       print("HERE7")
      }
   }
   print("HERE8")
-  plot(spiketrain[[1]]$trials[[100]][,"boxname"], spiketrain[[1]]$trials[[100]][,"Neuron1"], pch=19, col="darkgreen", cex=1.5)
-  lines(spiketrain[[1]]$trials[[100]][,"boxname"],spiketrain[[1]]$trials[[100]][,"Neuron2"], pch=19, col="blue", cex=1.5)
+  m <-as.matrix(spiketrain[[ses]]$trials[[100]]) 
+  par(mfrow=c(4,1))
+  barplot(as.numeric(m[,"Neuron1"]),col = "red",width = 0.3,xlim=c(0,10))
+  barplot(as.numeric(m[,"Neuron2"]),col = "red",width = 0.3,xlim=c(0,10))
+  barplot(as.numeric(m[,"Neuron3"]),col = "red",width = 0.3,xlim=c(0,10))
+  barplot(as.numeric(m[,"Sum"]),col = "red",width = 0.3,xlim=c(0,10))
 }

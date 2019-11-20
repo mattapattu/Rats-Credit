3f2d39cac5b74772851ecd3bff1838d25a611999
mattapattu <mattapattu@gmail.com>
2019-11-20 08:54:17 +0100

    latest

--- Sources/heatmap.R
+++ Sources/heatmap.R
@@ -7,7 +7,7 @@ plot.heatmap=function(enreg,rat){
   ###spikes 1s before reward, Reward, 1s after reward
   
   for(ses in c(1:2)){
-
+    
     last_trial <- as.numeric(enreg[[ses]]$POS[length(enreg[[ses]]$POS[,1]),"trial"])
     
     reward49_trials <- as.numeric(enreg[[ses]]$POS[which(enreg[[ses]]$POS[,"Reward"]== "49"),"trial"])
@@ -21,8 +21,11 @@ plot.heatmap=function(enreg,rat){
     neurons <- max(as.numeric(enreg[[ses]]$SPIKES[,"neuron"]))
     for(neuron in 1:neurons){
       mat <-matrix(0, last_trial, 15)
-      colnames(mat) <- c("a","b","b'","c","c'","d","e","e'","f","g","h","i","i'","j","k")
+      colnames(mat) <- c("A","B","B'","C","C'","D","E","E'","F","G","H","I","I'","J","K")
       trialIndex = 1
+      nSpikes <- matrix(0, last_trial, 15)
+      timesinBoxes <- matrix(0, last_trial, 15)
+      
       for(t in 1:last_trial){
         
         a <- which(enreg[[ses]]$POS[,"trial"] == t & enreg[[ses]]$POS[,"boxname"]== "a")
@@ -110,22 +113,35 @@ plot.heatmap=function(enreg,rat){
         time_k_0 = as.numeric(enreg[[ses]]$POS[k[length(k)],1]) - as.numeric(enreg[[ses]]$POS[k[1],1])
         
         if(length(time_a_0) >0) {
-          mat[trialIndex,1] =length(enreg[[ses]]$SPIKES[which( enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "a"),1])*1000/time_a_0
+          
+          nSpikes[trialIndex,1]=length(enreg[[ses]]$SPIKES[which( enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "a"),1])
+          timesinBoxes[trialIndex,1]=time_a_0
+          mat[trialIndex,1]=length(enreg[[ses]]$SPIKES[which( enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "a"),1])*1000/time_a_0
         }
         
         
         if(length(grep("a,.*b,.*c.*e",allpaths[trialIndex],value = FALSE))>0){
           if(length(time_b_0) >0) {
+            
+            nSpikes[trialIndex,2]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "b",1])
+            timesinBoxes[trialIndex,2]=time_b_0
             mat[trialIndex,2] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "b",1])*1000/time_b_0
           }
           if(length(time_c_0) >0) {
+            
+            nSpikes[trialIndex,4]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "c",1])
+            timesinBoxes[trialIndex,4]=time_c_0
             mat[trialIndex,4] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "c",1])*1000/time_c_0
           }
         }else if(length(grep("a,.*b,.*c.*i",allpaths[trialIndex],value = FALSE))>0){
           if(length(time_b_prime) >0) {
-              mat[trialIndex,3] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "b",1])*1000/time_b_prime
+            nSpikes[trialIndex,3]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "b",1])
+            timesinBoxes[trialIndex,3]=time_b_prime
+            mat[trialIndex,3] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "b",1])*1000/time_b_prime
           }
           if(length(time_c_prime) >0) {
+            nSpikes[trialIndex,5]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "c",1])
+            timesinBoxes[trialIndex,5]=time_c_prime
             mat[trialIndex,5] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "c",1])*1000/time_c_prime
           }
         }
@@ -133,43 +149,63 @@ plot.heatmap=function(enreg,rat){
         
         
         if(length(time_d_0) >0) {
+          nSpikes[trialIndex,6]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "d",1])
+          timesinBoxes[trialIndex,6]=time_d_0
           mat[trialIndex,6] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "d",1])*1000/time_d_0
         }
         
         
         if(t %in% reward49_trials){
           if(length(time_e_0) >0) {
+            nSpikes[trialIndex,7]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "e",1])
+            timesinBoxes[trialIndex,7]=time_e_0
             mat[trialIndex,7] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "e",1])*1000/time_e_0
           }
         }else{
           if(length(time_e_prime) >0) {
+            nSpikes[trialIndex,8]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "e",1])
+            timesinBoxes[trialIndex,8]=time_e_prime
             mat[trialIndex,8] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "e",1])*1000/time_e_prime
           }
         }
         
         if(length(time_f_0) >0) {
+          nSpikes[trialIndex,9]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "f",1])
+          timesinBoxes[trialIndex,9]=time_f_0
           mat[trialIndex,9] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "f",1])*1000/time_f_0
         }
         if(length(time_g_0) >0) {
+          nSpikes[trialIndex,10]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "g",1])
+          timesinBoxes[trialIndex,10]=time_g_0
           mat[trialIndex,10] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "g",1])*1000/time_g_0
         }
         if(length(time_h_0) >0) {
+          nSpikes[trialIndex,11]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "h",1])
+          timesinBoxes[trialIndex,11]=time_h_0
           mat[trialIndex,11] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "h",1])*1000/time_h_0
         }
         
-        if(t %in% reward51_trials){
+        if(t %in% reward51_trials) {
           if(length(time_i_0) >0) {
+            nSpikes[trialIndex,12]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "i",1])
+            timesinBoxes[trialIndex,12]=time_i_0
             mat[trialIndex,12] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "i",1])*1000/time_i_0
           }
         }else{
           if(length(time_i_prime) >0) {
+            nSpikes[trialIndex,13]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "i",1])
+            timesinBoxes[trialIndex,13]=time_i_prime
             mat[trialIndex,13] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "i",1])*1000/time_i_prime
           }
         }
         if(length(time_j_0) >0) {
+          nSpikes[trialIndex,14]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "j",1])
+          timesinBoxes[trialIndex,14]=time_j_0
           mat[trialIndex,14] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "j",1])*1000/time_j_0
         }
         if(length(time_k_0) >0) {
+          nSpikes[trialIndex,15]=length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "k",1])
+          timesinBoxes[trialIndex,15]=time_k_0
           mat[trialIndex,15] =length(enreg[[ses]]$SPIKES[ enreg[[ses]]$SPIKES[,"trial"]== t & enreg[[ses]]$SPIKES[,"neuron"]== neuron & enreg[[ses]]$SPIKES[,"boxName"]== "k",1])*1000/time_k_0
         }
         
@@ -177,6 +213,9 @@ plot.heatmap=function(enreg,rat){
       }
       
       library(ggplot2)
+      library(seriation)
+      library(mutoss)
+      
       
       longData<-melt(mat)
       mat[which(is.nan(mat))]<-0
@@ -185,7 +224,7 @@ plot.heatmap=function(enreg,rat){
       longData$Var1 <- factor(longData$Var1, (unlist(o[[1]][])))
       longData$Var2 <- factor(longData$Var2, names(unlist(o[[2]][])))
       longData<-longData[longData$value!=0,]
-      ggplot(longData, aes(x = Var2, y = Var1)) + 
+      ggplot(longData, aes(x = Var1, y = Var2)) + 
         geom_raster(aes(fill=value)) + 
         scale_fill_gradient(low="grey90", high="red") +
         labs(x="Boxes", y="Trials", title=paste('Heatmap_',rat,'_neuron_',neuron,'_ses_',ses,sep="")) +
@@ -195,10 +234,16 @@ plot.heatmap=function(enreg,rat){
       
       ggsave(paste('Heatmap_seriated_',rat,'_Neuron_',neuron,'_ses_',ses,'.png',sep=""), device = "png",width = 16, height = 9, dpi = 100)
       
+      pvals <- numeric()
+      for(i in 1:15){
+        l<-which(timesinBoxes[,i]!=0)
+        pvals <- c(pvals,chisq.test(nSpikes[l,i],p=timesinBoxes[l,i]/sum(timesinBoxes[l,i]))[[3]])
+      }
+      
       
     } 
-    }
-    
+  }
+  
   
   print("Returning from plot")
 }

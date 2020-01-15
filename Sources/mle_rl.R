e79c586e66504ce8a1580cdb0d58041866118257
mattapattu <mattapattu@gmail.com>
2020-01-15 14:08:22 +0100

    latest

--- Sources/mle_rl.R
+++ Sources/mle_rl.R
@@ -1,8 +1,11 @@
 
 
-#### Function to call for plotting heatmap
-mle_rl=function(enreg,rat,dirpath1,plot){
 
+#### Function to call for plotting heatmap
+mle_rl=function(enreg,rat){
+
+  allpaths <- matrix("",0,2)
+  colnames(allpaths) <- c("Path","Session")
   ### Loop through all enreg[[ses]] of current rat
   for(ses in 1:length(enreg)){
 
@@ -19,44 +22,51 @@ mle_rl=function(enreg,rat,dirpath1,plot){
     reward51_trials <- as.numeric(enreg[[ses]]$POS[which(enreg[[ses]]$POS[,"Reward"]== "51"),"trial"])
 
     r <- rle(enreg[[ses]]$POS[,"boxname"])
-    allpaths <- toString(r$values)
-    allpaths<-strsplit(allpaths,"(?<=[ei])(?=(, j, k)|(, f, g)|(, d, c)|(, h, c))",perl=TRUE)[[1]]
-    
-    ### CHANGE THIS:
-    allpaths <- allpaths[,1]
-    ###
-    est <- optim(c(0.1,0.8,0.1,0.8),rl_eg_negLogLik,lower=c(0,0,0,0),upper=c(1,1,1,1),allpaths=allpaths, Q=Q, E=E,method="L-BFGS-B")
-    est$pa
-    
+    allpaths_ses <- toString(r$values)
+    allpaths_ses<-strsplit(allpaths_ses,"(?<=[ei])(?=(, j, k)|(, f, g)|(, d, c)|(, h, c))",perl=TRUE)[[1]]
+    allpaths_ses <-cbind(allpaths_ses,ses)
+    colnames(allpaths_ses) <- c("Path","Session")
+    l<-list(allpaths,allpaths_ses[-1])
+    allpaths <- rbind(allpaths,allpaths_ses[-1,])
+
     
   }
+  allpaths = updatePathNb(allpaths)
+  
+  R=matrix(0,nrow=2,ncol=6)
+  colnames(R)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
+  rownames(R)<-c("E","I")
+  R[1,4]=1
+  R[2,4]=1
+  
+  ### Init Q
+  Q = matrix(0,nrow=2,ncol=6)
+  colnames(Q)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
+  rownames(Q)<-c("E","I")
+
+  
+  E=matrix(0,nrow=2,ncol=6)
+  colnames(E)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
+  rownames(E)<-c("E","I")
+  
+
+  est <- optim(c(0.1,0.8,0.1,0.8,0.5,0.5),rl_eg_negLogLik,lower=c(0,0,0,0,0,0),upper=c(1,1,1,1,1,1),allpaths=allpaths, Q=Q, E=E, method="L-BFGS-B")
+  print(sprintf("Estimated parameters for rat %s = %s",rat,est$par ))
+}
+
+
+updatePathNb=function(allpaths){
+  allpaths <- cbind(allpaths,0)
+  for(i in 1:(length(allpaths[,1]))){
+    allpaths[i,3] = getPathNumber(allpaths[i,1])
+  }
+    return(allpaths)
 }
 
 
 
-R=matrix(0,nrow=2,ncol=6)
-colnames(R)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
-rownames(R)<-c("E","I")
-R[1,4]=1
-R[2,4]=1
 
-### Init Q
-Q = matrix(0,nrow=2,ncol=6)
-colnames(Q)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
-rownames(Q)<-c("E","I")
-Q[1,1]=0.5
-Q[2,1]=0.5
-
-E=matrix(0,nrow=2,ncol=6)
-colnames(E)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
-rownames(E)<-c("E","I")
-
-QProb=matrix(0,nrow=2,ncol=6)
-colnames(QProb)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
-rownames(QProb)<-c("E","I")
-
-
-getPathNb=function(path){
+getPathNumber=function(path){
   path  = gsub("^, ","",path)
   
   if(grepl("^d.*c.*h.*i",path)){
@@ -83,6 +93,9 @@ getPathNb=function(path){
     pathnb = 6
   }else if(grepl("^.*i$",path)){
     pathnb = 6
+  }else{
+    ## A =7
+    pathnb=6
   }
   
   return(pathnb)
@@ -90,24 +103,29 @@ getPathNb=function(path){
 
 ### Action = Path
 ## State = E or I
-sarsa_mle=function(alpha,epsilon,gamma,lambda,allpaths,Q,E){
+sarsa_mle=function(alpha,epsilon,gamma,lambda,path1_prb1,path1_prb2,allpaths,Q,E){
   
   actions <-list()
   states <-list()
   
   QProb <-numeric()
+  Q[1,1]=path1_prb1
+  Q[2,1]=path1_prb2
   ## Ignore first path
   ## Start from the end of path1
   
-  if(grepl("^.*e$",allpaths[1])){
+  if(grepl("^.*e$",allpaths[1,1])){
     S = 1
-  }else if(grepl("^.*i$",allpaths[1])){
+  }else if(grepl("^.*i$",allpaths[1,1])){
     S = 2
   }else{
     print("Unknown intial state. Check")
   }
   
-  A  = getPathNb(allpaths[2])
+  A  = as.numeric(allpaths[1,3])
+  if(A==7){
+    as.numeric(allpaths[1,3])
+  }
   episode=1
   actions[[episode]] <- vector()
   
@@ -116,7 +134,7 @@ sarsa_mle=function(alpha,epsilon,gamma,lambda,allpaths,Q,E){
   returnToInitState = F
   
 
-  for(i in 2:(length(allpaths)-1)){
+  for(i in 2:(length(allpaths[,1])-1)){
 
     if(A==which.max(Q[S,])){
       QProb <- c(QProb,(1-epsilon +epsilon/6))
@@ -133,8 +151,9 @@ sarsa_mle=function(alpha,epsilon,gamma,lambda,allpaths,Q,E){
       reward=0
     }
     
-    S_prime=getNextState(S,A)
-    A_prime=getPathNb(allpaths[i+1])
+    ## Next state is the box where current action ends
+    S_prime=getNextState(allpaths,i)
+    A_prime=as.numeric(allpaths[i+1,3])
     E[S,A]=E[S,A]+1
     delta=reward+(gamma* Q[S_prime,A_prime]) - Q[S,A]
     Q=Q+ (alpha*delta*E)
@@ -179,16 +198,27 @@ sarsa_mle=function(alpha,epsilon,gamma,lambda,allpaths,Q,E){
 
 
 
-getNextState=function(curr_state,action){
-  if(action == 5){
-    new_state=curr_state
-  }else if(curr_state==1){
-    new_state=2
-  }else if(curr_state==2){
-    new_state=1
+getNextState=function(allpaths,i){
+  if(grepl("^.*e",allpaths[i,1])){
+    next_state = 1
+  }else if(grepl("^.*i",allpaths[i,1])){
+    next_state = 2
+  }
+  ### If allpaths[i,] does not end in E/I , use the begining of next path to find the new state 
+  else{
+    if(i<length(allpaths[i,])){
+      if(grepl("^, f",allpaths[i+1,1])||grepl("^, d",allpaths[i+1,1])){
+        next_state = 1
+      }else if(grepl("^, j",allpaths[i+1,1])||grepl("^, h",allpaths[i+1,1])){
+        next_state = 2
+      }
+    }else{
+      next_state=-1
+    }
+    
   }
 
-  return(new_state)
+  return(next_state)
 }
 
 
@@ -197,7 +227,9 @@ rl_eg_negLogLik <- function(par,allpaths,Q,E) {
   gamma <- par[2]
   epsilon <- par[3]
   lambda <- par[4]
-  lik <- sarsa_mle(alpha,epsilon,gamma,lambda,allpaths,Q,E)
+  path1_prb1 <- 0.5
+  path1_prb2 <- 0.5
+  lik <- sarsa_mle(alpha,epsilon,gamma,lambda,path1_prb1,path1_prb2,allpaths,Q,E)
   negLogLik <- -sum(log(lik))
   return(negLogLik)
 }

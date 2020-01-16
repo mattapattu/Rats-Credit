7685dfd573ea659abe627427383e10d7e1f24b29
mattapattu <mattpattu@gmail.com>
2020-01-16 12:40:32 +0100

    latest

--- Sources/rl_aca.R
+++ Sources/rl_aca.R
@@ -2,13 +2,13 @@
 library(GenSA)
 
 #### Function to call for plotting heatmap
-mle_rl=function(enreg,rat){
-
+mle_aca=function(enreg,rat){
+  
   allpaths <- matrix("",0,2)
   colnames(allpaths) <- c("Path","Session")
   ### Loop through all enreg[[ses]] of current rat
   for(ses in 1:length(enreg)){
-
+    
     if(is.null(enreg[[ses]])){
       print(sprintf("skipping %s ses %i as enreg is empty",rat,ses))
       next
@@ -16,11 +16,11 @@ mle_rl=function(enreg,rat){
       print(sprintf("skipping %s ses %i as reward data is empty",rat,ses))
       next
     }
-
+    
     last_trial <- as.numeric(enreg[[ses]]$POS[length(enreg[[ses]]$POS[,1]),"trial"])
     reward49_trials <- as.numeric(enreg[[ses]]$POS[which(enreg[[ses]]$POS[,"Reward"]== "49"),"trial"])
     reward51_trials <- as.numeric(enreg[[ses]]$POS[which(enreg[[ses]]$POS[,"Reward"]== "51"),"trial"])
-
+    
     r <- rle(enreg[[ses]]$POS[,"boxname"])
     allpaths_ses <- toString(r$values)
     allpaths_ses<-strsplit(allpaths_ses,"(?<=[ei])(?=(, j, k)|(, f, g)|(, d, c)|(, h, c))",perl=TRUE)[[1]]
@@ -28,45 +28,45 @@ mle_rl=function(enreg,rat){
     colnames(allpaths_ses) <- c("Path","Session")
     l<-list(allpaths,allpaths_ses[-1])
     allpaths <- rbind(allpaths,allpaths_ses[-1,])
-
+    
     
   }
   allpaths = updatePathNb(allpaths)
   
-  R=matrix(0,nrow=2,ncol=6)
-  colnames(R)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
-  rownames(R)<-c("E","I")
-  R[1,4]=1
-  R[2,4]=1
   
-  ### Init Q
-  Q = matrix(0,nrow=2,ncol=6)
-  colnames(Q)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
-  rownames(Q)<-c("E","I")
-
+  ### Init H
+  H = matrix(0,nrow=2,ncol=6)
+  colnames(H)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
+  rownames(H)<-c("E","I")
   
-  E=matrix(0,nrow=2,ncol=6)
-  colnames(E)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
-  rownames(E)<-c("E","I")
+  ### Init S - for scores
+  Scores = matrix(0,nrow=2,ncol=6)
+  colnames(Scores)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
+  rownames(Scores)<-c("E","I")
+  
+  ## Counter for Actions
+  V = matrix(0,nrow=2,ncol=6)
+  colnames(V)<-c("Path1","Path2","Path3","CorrPath","WM-Path","Unknown-Paths")
+  rownames(V)<-c("E","I")
+  
   
   #out <- GenSA(lower = c(0.0,0.0,0.0,0.0), upper = c(1,1,1,1), fn = rl_eg_negLogLik,allpaths=allpaths, Q=Q, E=E,control = list(max.time=600, verbose=TRUE))
   
   #est <- optim(c(0.1,0.8,0.1,0.8,0.5,0.5),rl_eg_negLogLik,lower=c(0,0,0,0,0,0),upper=c(1,1,1,1,1,1),allpaths=allpaths, Q=Q, E=E, method="L-BFGS-B")
   #print(sprintf("Estimated parameters for rat %s = %s",rat,est$par ))
   minima=Inf
-  optimal_vals <-numeric()
-  for(alpha in seq(0.1,1,0.1)){
-    for(gamma in seq(0.1,1,0.1)){
-      for(epsilon in seq(0.1,1,0.1)){
-        for(lambda in seq(0.1,1,0.1)){
-          for(path1_prb1 in seq(0.1,1,0.1)){
-            for(path1_prb2 in seq(0.1,1,0.1)){
+  
+  for(alpha in seq(0,1,0.1)){
+    for(gamma in seq(0,1,0.1)){
+      for(epsilon in seq(0,1,0.1)){
+        for(lambda in seq(0,1,0.1)){
+          for(path1_prb1 in seq(0,1,0.1)){
+            for(path1_prb2 in seq(0,1,0.1)){
               
-              log_likelihood=rl_eg_negLogLik(c(alpha,gamma,epsilon,lambda,path1_prb1,path1_prb2),allpaths,Q,E)
-               if(log_likelihood < minima){
-                 minima=log_likelihood
-                 optimal_vals <- c(alpha,gamma,epsilon,lambda,path1_prb1,path1_prb2)
-               }
+              log_likelihood=rl_eg_negLogLik(c(alpha,gamma,epsilon,lambda),allpaths,Q,E)
+              if(log_likelihood < minima){
+                minima=log_likelihood
+              }
             }
             
           }
@@ -75,8 +75,7 @@ mle_rl=function(enreg,rat){
       }
     }
   }
-  print(minima)
-  print(optimal_vals)
+  
   
 }
 
@@ -86,7 +85,7 @@ updatePathNb=function(allpaths){
   for(i in 1:(length(allpaths[,1]))){
     allpaths[i,3] = getPathNumber(allpaths[i,1])
   }
-    return(allpaths)
+  return(allpaths)
 }
 
 
@@ -129,14 +128,12 @@ getPathNumber=function(path){
 
 ### Action = Path
 ## State = E or I
-sarsa_mle=function(alpha,epsilon,gamma,lambda,path1_prb1,path1_prb2,allpaths,Q,E){
+aca_mle=function(alpha,n,allpaths,Q,E){
   
   actions <-list()
   states <-list()
   
   QProb <-numeric()
-  Q[1,1]=path1_prb1
-  Q[2,1]=path1_prb2
   ## Ignore first path
   ## Start from the end of path1
   
@@ -149,41 +146,37 @@ sarsa_mle=function(alpha,epsilon,gamma,lambda,path1_prb1,path1_prb2,allpaths,Q,E
   }
   
   A  = as.numeric(allpaths[1,3])
-  if(A==7){
-    as.numeric(allpaths[1,3])
-  }
+ 
   episode=1
   actions[[episode]] <- vector()
+  states[[episode]] <- vector()
   
   initState=S
   changeState = F
   returnToInitState = F
+  reward=0
   
-
   for(i in 2:(length(allpaths[,1])-1)){
-
-    if(A==which.max(Q[S,])){
-      QProb <- c(QProb,(1-epsilon +epsilon/6))
-    }else{
+    
+    if(i<=n){
       QProb <- c(QProb,(epsilon/6))
+    }else{
+      QProb <- c(QProb,softmax(A,S,H))
     }
     
     l<-which(as.numeric(enreg[[ses]]$POS[,"trial"])==i)
     R=sum(as.numeric(enreg[[ses]]$POS[l,"Reward"]))
-    reward=0
     if(R>0){
-      reward=1
+      reward=reward+1
     }else{
-      reward=0
+      reward=reward+0
     }
     
     ## Next state is the box where current action ends
     S_prime=getNextState(allpaths,i)
     A_prime=as.numeric(allpaths[i+1,3])
-    E[S,A]=E[S,A]+1
-    delta=reward+(gamma* Q[S_prime,A_prime]) - Q[S,A]
-    Q=Q+ (alpha*delta*E)
-    E=E*gamma*lambda
+
+   
     
     if(A == 4 & S == 1){
       actions[[episode]] <- append(actions[[episode]],51)
@@ -193,7 +186,7 @@ sarsa_mle=function(alpha,epsilon,gamma,lambda,path1_prb1,path1_prb2,allpaths,Q,E
       actions[[episode]] <- append(actions[[episode]],unname(A))
     }
     #actions <- c(actions,sprintf("S%i-P%i",S,A))
-    states <- c(states,S)
+    states[[episode]] <- append(states[[episode]],unname(S))
     #print(sprintf("Current state = %i, Action = %i", S,A))
     if(S_prime!=initState){
       changeState = T
@@ -203,20 +196,56 @@ sarsa_mle=function(alpha,epsilon,gamma,lambda,path1_prb1,path1_prb2,allpaths,Q,E
       #print(sprintf("Setting returnToInitState to T"))
     }
     
-    S=S_prime
-    A=A_prime
-    
+   
+    ## Check if episode ended
     if(returnToInitState){
       changeState = F
       returnToInitState = F
       initState=S
-      E=E*0
-      episode  = episode+1
+      
+      a<-actions[[episode]]
+      s<-states[[episode]]
+      
+      for(state in 1:2){
+        for(action in 1:6){
+          ## If S,A is visited in the episode
+          if(any(s==state) && any(a==action)){
+            if(i<=n){
+              ### Credit = Score * activity
+              ## Activity of (A,S) = #Nb of times Action A is taken in State S/ # Nb of times State S is visited
+              H[state,action]=reward*V[state,action]/rowsum(V[state,])
+            }else{
+              ## expected_score = E(Score of Action A in State S) = #Total Score of Action A in State S before current trial/#Nb of times Action A is taken in State S
+              expected_score = Score[state,action]/V[state,action]
+              H[state,action]=H[state,action]+alpha*(reward-expected_score)*(1-softmax(action,state,H))
+            }
+          }## If S,A is not visited in the episode
+          else{
+            if(i<=n){
+              ### No Credit update as reward =0
+            }else{
+              expected_score = Score[state,action]/V[state,action]
+              H[state,action]=H[state,action]-alpha*(reward-expected_score)*(softmax(action,state,H))
+            }
+          }
+          
+          ## Update Score of current Action A in current state S
+          Score[state,action]=Score[state,action]+reward
+          V[state,action]=V[state,action]+1
+        }
+      }
+      ## reset rewards
+      reward=0
+      episode = episode+1
+      
       #print(sprintf("Updating episode to %i",episode))
       if(i < length(allpaths)-1){
         actions[[episode]] <- vector()
       }
     }
+    
+    S=S_prime
+    A=A_prime
   }
   return(QProb)
 }
@@ -243,7 +272,7 @@ getNextState=function(allpaths,i){
     }
     
   }
-
+  
   return(next_state)
 }
 
@@ -258,6 +287,11 @@ rl_eg_negLogLik <- function(par,allpaths,Q,E) {
   lik <- sarsa_mle(alpha,epsilon,gamma,lambda,path1_prb1,path1_prb2,allpaths,Q,E)
   negLogLik <- -sum(log(lik))
   return(negLogLik)
+}
+
+softmax=function(A,S,H){
+  pr_A=exp(H[S,A])/rowsum(H[S,])
+  return(pr_A)
 }
 
 
